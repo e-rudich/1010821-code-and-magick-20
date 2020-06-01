61e4c99de355e3010ccff4eafb9147cb2d9d66b3
Elena <e.d.rudich@gmail.com>
2020-06-01 22:16:14 +0300

    добавляет функции отрисовки текста, бара, гистограммы

--- js/stat.js
+++ js/stat.js
@@ -5,6 +5,11 @@ var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var CLOUD_GAP = 10;
+var CLOUD_COLOR = '#fff';
+var SHADOW_COLOR = 'rgba(0, 0, 0, 0.3)';
+var TEXT_COLOR = '#000';
+var TEXT_STYLE = '16px PT Mono';
+
 var FONT_GAP = 20;
 var TEXT_HEIGHT = 35;
 var BAR_WIDTH = 40;
@@ -16,58 +21,54 @@ var renderCloud = function (ctx, x, y, color) {
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-var getMaxElement = function (arr) {
-  var maxElement = arr[0];
+var renderText = function (ctx, font, color, text, x, y) {
+  ctx.font = font;
+  ctx.textBaseline = 'hanging';
+  ctx.fillStyle = color;
+  ctx.fillText(text, x, y);
+};
 
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] > maxElement) {
-      maxElement = arr[i];
-    }
-  }
-
-  return maxElement;
+var getMaxElement = function (items) {
+  return items.reduce(function (accumulator, currentValue) {
+    return currentValue > accumulator ? currentValue : accumulator;
+  }, -Infinity);
 };
 
 var getRandom = function (max) {
   return Math.round(Math.random() * Math.floor(max));
 };
 
-window.renderStatistics = function (ctx, players, times) {
-  renderCloud(ctx, CLOUD_X + CLOUD_GAP, CLOUD_Y + CLOUD_GAP, 'rgba(0, 0, 0, 0.3)');
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+var renderBar = function (ctx, barCoordinateX, barCoordinateY, barWidth, barHeight) {
+  ctx.fillRect(barCoordinateX, barCoordinateY, barWidth, barHeight);
+};
 
-  ctx.font = '16px PT Mono';
-  ctx.textBaseline = 'hanging';
-  ctx.fillStyle = '#000';
-  ctx.fillText('Ура вы победили!', CLOUD_X + FONT_GAP, CLOUD_Y + FONT_GAP);
-  ctx.fillText('Список результатов:', CLOUD_X + FONT_GAP, CLOUD_Y + FONT_GAP * 2);
+var renderChart = function (ctx, arrayPlayers, arrayTimes) {
+  var maxTime = getMaxElement(arrayTimes);
 
-  var maxTime = getMaxElement(times);
+  arrayPlayers.forEach(function (item, i, players) {
+    var barHeight = (BAR_MAX_HEIGHT * arrayTimes[i]) / maxTime;
+    var barWidth = BAR_WIDTH;
+    var coordinateX = CLOUD_X + BAR_GAP + (BAR_GAP + BAR_WIDTH) * i;
+    var coordinateY = CLOUD_HEIGHT - barHeight - TEXT_HEIGHT;
 
-  for (var i = 0; i < players.length; i++) {
-    var barHeight = (BAR_MAX_HEIGHT * times[i]) / maxTime;
-    ctx.fillStyle = '#000';
-    ctx.fillText(Math.round(times[i]), CLOUD_X + BAR_GAP + (BAR_GAP + BAR_WIDTH) * i, CLOUD_HEIGHT - barHeight - TEXT_HEIGHT - FONT_GAP);
-    ctx.fillText(players[i], CLOUD_X + BAR_GAP + (BAR_GAP + BAR_WIDTH) * i, CLOUD_HEIGHT - FONT_GAP);
+    ctx.fillStyle = TEXT_COLOR;
+    ctx.fillText(Math.round(arrayTimes[i]), coordinateX, coordinateY - FONT_GAP);
+    ctx.fillText(players[i], coordinateX, CLOUD_HEIGHT - FONT_GAP);
 
     ctx.fillStyle = players[i] === 'Вы' ? 'rgba(255, 0, 0, 1)' : 'hsl(240, ' + getRandom(100) + '%, 50%)';
 
-    ctx.fillRect(CLOUD_X + BAR_GAP + (BAR_GAP + BAR_WIDTH) * i, CLOUD_HEIGHT - barHeight - TEXT_HEIGHT, BAR_WIDTH, barHeight);
-  }
+    renderBar(ctx, coordinateX, coordinateY, barWidth, barHeight);
+  });
 };
 
-// window.renderStatistics = function (ctx) {
-//   ctx.fillStyle = '#fff';
-//   ctx.beginPath();
-//   ctx.lineTo(110, 20);
-//   ctx.lineTo(110, 290);
-//   ctx.lineTo(530, 290);
-//   ctx.lineTo(530, 20);
-//   ctx.bezierCurveTo(530, 20, 470, 0, 410, 20);
-//   ctx.bezierCurveTo(410, 20, 360, 0, 310, 20);
-//   ctx.bezierCurveTo(310, 20, 210, 0, 110, 20);
-//   ctx.lineTo(400, 20);
-//   ctx.closePath();
-//   ctx.fill();
-// };
-// не придумала как это записать в renderCloud
+
+
+window.renderStatistics = function (ctx, players, times) {
+  renderCloud(ctx, CLOUD_X + CLOUD_GAP, CLOUD_Y + CLOUD_GAP, SHADOW_COLOR);
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_COLOR);
+
+  renderText(ctx, TEXT_STYLE, TEXT_COLOR, 'Ура вы победили!', CLOUD_X + FONT_GAP, CLOUD_Y + FONT_GAP);
+  renderText(ctx, TEXT_STYLE, TEXT_COLOR, 'Список результатов:', CLOUD_X + FONT_GAP, CLOUD_Y + FONT_GAP * 2);
+
+  renderChart(ctx, players, times);
+};
